776eaa6e6fc66f99289f33430759b4dc79d45523
flowering-time-design <selinani1117@gmail.com>
2023-03-08 19:02:14 +0800

    js

--- js/flower.js
+++ js/flower.js
@@ -1,6 +1,6 @@
 // button switch
-$("button").click(function () {
-  $("button").removeClass("active");
+$(".img-cover button").click(function () {
+  $(".img-cover button").removeClass("active");
   $(this).addClass("active");
 });
 
@@ -159,6 +159,54 @@ bigImg.addEventListener("load", function () {
 
 bigImg.src =
   "https://raw.githubusercontent.com/flowering-time-design/flowering-time-design/master/imgs/take-photo.jpg";
+
+// gallery
+//lightbox オプションの設定※https://lokeshdhakar.com/projects/lightbox2/#options参照
+
+lightbox.option({
+  wrapAround: true, //グループ最後の写真の矢印をクリックしたらグループ最初の写真に戻る
+  albumLabel: " %1 / total %2 ", //合計枚数中現在何枚目かというキャプションの見せ方を変更できる
+});
+
+//ふわっと見せるためのJS。3-5-3 ページが読み込まれたらすぐに動かしたい&画面をスクロールをしたら動かしたい場合内のソースコード使用
+
+function fadeAnime() {
+  // flipLeft
+  $(".gallery li").each(function () {
+    var elemPos = $(this).offset().top;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass("flipLeft");
+    } else {
+      $(this).removeClass("flipLeft");
+    }
+  });
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  fadeAnime(); /* アニメーション用の関数を呼ぶ*/
+}); // ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on("load", function () {
+  fadeAnime(); /* アニメーション用の関数を呼ぶ*/
+}); // ここまでページが読み込まれたらすぐに動かしたい場合の記述
+
+// slider
+$(".slider").slick({
+  autoplay: true, //自動的に動き出すか。初期値はfalse。
+  infinite: true, //スライドをループさせるかどうか。初期値はtrue。
+  speed: 500, //スライドのスピード。初期値は300。
+  slidesToShow: 3, //スライドを画面に3枚見せる
+  slidesToScroll: 1, //1回のスクロールで1枚の写真を移動して見せる
+  prevArrow: '<div class="slick-prev"></div>', //矢印部分PreviewのHTMLを変更
+  nextArrow: '<div class="slick-next"></div>', //矢印部分NextのHTMLを変更
+  centerMode: true, //要素を中央ぞろえにする
+  variableWidth: true, //幅の違う画像の高さを揃えて表示
+  dots: true, //下部ドットナビゲーションの表示
+});
 
 /* locomotive-scroll-master */
 /* 放在menu上面 */
