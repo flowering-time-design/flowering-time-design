0f1d88adec426988927cbf94c56e3c5f9c022591
flowering-time-design <selinani1117@gmail.com>
2023-03-19 12:34:27 +0800

    renew

--- js/flower.js
+++ js/flower.js
@@ -160,10 +160,28 @@ bigImg.addEventListener("load", function () {
 bigImg.src =
   "https://raw.githubusercontent.com/flowering-time-design/flowering-time-design/master/imgs/take-photo.jpg";
 
+// gallery
+
 lightbox.option({
   wrapAround: true, //グループ最後の写真の矢印をクリックしたらグループ最初の写真に戻る
   albumLabel: " %1 / total %2 ", //合計枚数中現在何枚目かというキャプションの見せ方を変更できる
 });
+
+//ふわっと見せるためのJS。3-5-3 ページが読み込まれたらすぐに動かしたい&画面をスクロールをしたら動かしたい場合内のソースコード使用
+
+function fadeAnime() {
+  // flipLeft
+  $(".gallery li").each(function () {
+    var elemPos = $(this).offset().top;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass("animate__fadeInOpacity");
+    } else {
+      $(this).removeClass("animate__fadeInOpacity");
+    }
+  });
+}
 
 // 画面をスクロールをしたら動かしたい場合の記述
 $(window).scroll(function () {
