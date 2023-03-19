669cc94e564a2847d2cc3253695d82d1be71f4be
flowering-time-design <selinani1117@gmail.com>
2023-03-19 12:12:17 +0800

    about

--- js/flower.js
+++ js/flower.js
@@ -160,29 +160,10 @@ bigImg.addEventListener("load", function () {
 bigImg.src =
   "https://raw.githubusercontent.com/flowering-time-design/flowering-time-design/master/imgs/take-photo.jpg";
 
-// gallery
-//lightbox オプションの設定※https://lokeshdhakar.com/projects/lightbox2/#options参照
-
 lightbox.option({
   wrapAround: true, //グループ最後の写真の矢印をクリックしたらグループ最初の写真に戻る
   albumLabel: " %1 / total %2 ", //合計枚数中現在何枚目かというキャプションの見せ方を変更できる
 });
-
-//ふわっと見せるためのJS。3-5-3 ページが読み込まれたらすぐに動かしたい&画面をスクロールをしたら動かしたい場合内のソースコード使用
-
-function fadeAnime() {
-  // flipLeft
-  $(".gallery li").each(function () {
-    var elemPos = $(this).offset().top;
-    var scroll = $(window).scrollTop();
-    var windowHeight = $(window).height();
-    if (scroll >= elemPos - windowHeight) {
-      $(this).addClass("animate__fadeInOpacity");
-    } else {
-      $(this).removeClass("animate__fadeInOpacity");
-    }
-  });
-}
 
 // 画面をスクロールをしたら動かしたい場合の記述
 $(window).scroll(function () {
@@ -196,11 +177,16 @@ $(window).on("load", function () {
 
 // slider
 $(".slider").slick({
-  autoplay: true, //自動的に動き出すか。初期値はfalse。
-  infinite: true, //スライドをループさせるかどうか。初期値はtrue。
-  speed: 500, //スライドのスピード。初期値は300。
-  slidesToShow: 3, //スライドを画面に3枚見せる
-  slidesToScroll: 1, //1回のスクロールで1枚の写真を移動して見せる
+  autoplay: true,
+  //自動的に動き出すか。初期値はfalse。
+  infinite: true,
+  //スライドをループさせるかどうか。初期値はtrue。
+  speed: 500,
+  //スライドのスピード。初期値は300。
+  slidesToShow: 3,
+  //スライドを画面に3枚見せる
+  slidesToScroll: 1,
+  //1回のスクロールで1枚の写真を移動して見せる
   prevArrow: '<div class="slick-prev"></div>', //矢印部分PreviewのHTMLを変更
   nextArrow: '<div class="slick-next"></div>', //矢印部分NextのHTMLを変更
   centerMode: true, //要素を中央ぞろえにする
